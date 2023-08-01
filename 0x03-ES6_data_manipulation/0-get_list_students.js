55f6c6cd359dc076cdecfe1b1f791149d077d16e
Murphy Lomboli <113598303+MurphyLomboli@users.noreply.github.com>
2023-08-01 10:18:44 +0200

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,14 +1,3 @@
-// a function named getListStudents that returns an array of objects.
-
-// Each object should have three attributes: id (Number), firstName (String), and location (String).
-
-// The array contains the following students in order:
-
-// Guillaume, id: 1, in San Francisco
-// James, id: 2, in Columbia
-// Serena, id: 5, in San Francisco
-
-/* eslint-disable */
 const getListStudents =() => {
   const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
